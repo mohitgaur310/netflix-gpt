38010bfcaab95ccd7d507ee7f8ed42e49e9b25a6
mohit <mohitgaur310@gmail.com>
2023-09-05 02:52:00 +0530

    configured Tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="p-4 m-52 text-center font-semibold text-gray-700 ">
+      <h1 className=''> Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
